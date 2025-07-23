92f10b45e579b51a36ca3fb5c0c7282075fdd210
TheDoanator <kdoan0625@gmail.com>
2025-07-23 13:58:12 -0500

    Fetch list of files up to any quantity

--- parser.py
+++ parser.py
@@ -5,6 +5,7 @@ from googleapiclient.discovery import build
 from google.cloud import generativelanguage
 
 SCOPES = ['https://www.googleapis.com/auth/drive']
+TO_BE_PROCESSED_FOLDER_ID = 'REMOVED'
 
 def get_google_drive_service():
   creds: None
@@ -12,6 +13,19 @@ def get_google_drive_service():
     creds = service_account.Credentials.from_service_account_file('service_account.json', scopes=SCOPES)
   return build('drive', 'v3', credentials=creds)
     
-
+    
 def main():
-  service = get_google_drive_service();
+  service = get_google_drive_service()
+  
+  full_results = []
+  results = service.files().list(q=f"'{TO_BE_PROCESSED_FOLDER_ID}' in parents", pageSize=1000, fields="files(id, name)", supportsAllDrives=True, includeItemsFromAllDrives=True).execute()
+  next_page_token = results.get('nextPageToken')
+  full_results = (results.get('files', []))
+  
+  while (next_page_token != None):
+      results = service.files().list(q=f"'{TO_BE_PROCESSED_FOLDER_ID}' in parents", pageSize=1000, pageToken=next_page_token, fields="files(id, name)", supportsAllDrives=True, includeItemsFromAllDrives=True).execute()
+      next_page_token = results.get('nextPageToken')
+      full_results.extend(results.get('files', []))
+  
+if __name__ == '__main__':
+    main()
